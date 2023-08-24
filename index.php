e7958340c6e7b5e04ebdad19c0a3ee6891083eec
prachisv04 <prachisvavhal@gmail.com>
2023-08-25 00:42:32 +0530

    delete left

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     $insert=false;
+    $update = false;
+$delete = false;
         // Connect to database 
 
         $servername = "localhost";
@@ -14,10 +16,11 @@
 
 <?php
           die();
-        
         }
         else{
-          if($_SERVER['REQUEST_METHOD']=='POST'){
+
+
+          if($_SERVER['REQUEST_METHOD']=='POST' AND  isset($_POST['title'])){
             $title = $_POST["title"];
             $description = $_POST["desc"];
             if($title!=""){
@@ -30,13 +33,48 @@
                   $_POST["desc"]="";
               }
               else{
-                  echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+                  echo "The record was not inserted  because of this error ---> ". mysqli_error($conn);
+              } 
+              }
+          }
+        if(isset($_POST['editId']))
+          {
+            $id = $_POST['editId'];
+            $title = $_POST["edittitle"];
+            $description = $_POST["editdesc"];
+            if($title!=""){
+              $sql = "UPDATE `notes` SET `Title`='$title' , `Description` = '$description' WHERE `notes`.`Note_id`='".substr($id,1)."'" ;
+             $result = mysqli_query($conn, $sql);
+            
+              if($result){ 
+                  $update = true;
+                  $_POST["edittitle"]="";
+                  $_POST["editdesc"]="";
+              }
+              else{
+                  echo "The record was not inserted  because of this error ---> ". mysqli_error($conn);
               } 
               }
             }
         
-        }
         
+        if(isset($_POST['del']))
+          {
+            $sno = $_POST['delId'];
+            
+            $sql = "DELETE FROM `Notes` WHERE `Note_id`='$sno'";
+                $result = mysqli_query($conn, $sql);
+                    
+                      if($result){ 
+                          $delete = true;
+                      }
+                      else{
+                          echo "The record was not deleted  because of this error ---> ". mysqli_error($conn);
+                      } 
+          }
+        
+        }
+      
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,7 +88,7 @@
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link href="CSS/style.css" rel="stylesheet">
   <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-  <<!-- Option 1: Include in HTML -->
+  <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
@@ -58,29 +96,34 @@
 </head>
 
 <body class="bg-dark">
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="editModal">Edit Note</h1>
+          <h1 class="modal-title fs-5" id="editModalTitle">Edit Note</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        <form action="/TO_DO/index.php" method="POST">
         <div class="modal-body">
           <div class="card-body">
+            
+            <div class="mb-3"> 
+            <input type="text" hidden class="form-control" id="editId" name="editId">
+            </div>
+
             <div class="mb-3">
-              <label for="title" class="form-label">Title</label>
-              <input type="text" class="form-control" id="titke" name="title">
+              <label for="edittitle" class="form-label">Title</label>
+              <input type="text" class="form-control" id="edittitle" name="edittitle">
             </div>
             <div class="mb-3">
-              <label for="desc" class="form-label">Description</label>
-              <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+              <label for="editdesc" class="form-label">Description</label>
+              <textarea class="form-control" id="editdesc" name="editdesc" rows="3"></textarea>
             </div>
           </div>
-        </div>
-        <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
         </div>
+        </form>
       </div>
     </div>
   </div>
@@ -128,7 +171,7 @@
           <form action="/TO_DO/index.php" method="POST">
             <div class="form-group mb-2 mt-2">
               <label for="title">Note Title</label>
-              <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+              <input type="text" class="form-control" id="title" name="title" aria-describedby="note_title">
             </div>
       
             <div class="form-group mb-2 mt-2">
@@ -148,6 +191,7 @@
                 <th scope="col">Title</th>
                 <th scope="col">Description</th>
                 <th scope="col">Actions</th>
+                <th scope="col"></th>
               </tr>
             </thead>
             <tbody>
@@ -161,8 +205,15 @@
                   <td>".$row['Title']."</td>
                   <td>".$row['Description']."</td>
                   <td>
-                    <button class='edit btn btn-primary btn-circle btn-sm' data-bs-toggle='modal' data-bs-target='#editModal'><i class='bi bi-pencil '></i></button>
-                    <button class='delete btn btn-danger btn-circle btn-sm'><i class='bi bi-x-circle'></i></button>
+                    <button class='edit btn btn-primary btn-circle btn-sm' data-bs-toggle='modal'  data-bs-target='#editModal'><i class='bi bi-pencil' id=e".$row['Note_id']."></i></button>
+                  </td>
+                  <td>
+                  <form method='POST'>
+                    <input type='hidden' id='delId' name='delId'>
+                    <button  class='delete btn btn-danger btn-circle btn-sm' >
+                    <i class='bi bi-x-circle' name='del' id=d".$row['Note_id']."></i>
+                    </button>
+                   </form>
                   </td>
                 </tr>";
               }
@@ -189,6 +240,38 @@
           $('#myTable').DataTable();
 
         });
+      </script>
+
+      <!-- yaha hum edit ke liye ek listener likhenge and usme se noteid title and desc set karenge -->
+      <script>
+          //edit selected row
+          edits = document.getElementsByClassName("edit");
+          Array.from(edits).forEach((element)=>{
+            element.addEventListener("click",(e)=>{
+              //extract value from tags
+              console.log(e.target);
+               let row = e.target.parentNode.parentNode.parentNode;
+               let title = row.getElementsByTagName("td")[0].innerText;
+               let description = row.getElementsByTagName("td")[1].innerText;
+              console.log(title,description);
+              //set value to modal tags
+              edittitle.value = title;
+              editdesc.value = description;
+              editId.value = e.target.id;
+              console.log("id is",e.target.id);
+            })//event listener
+          });// foreach end
+
+          //delete selected row
+          deletes = document.getElementsByClassName("delete");
+          Array.from(deletes).forEach((element)=>{
+            element.addEventListener("click",(e)=>{
+
+              editId = e.target.id.substr(1);
+              delId.value = editId;
+              console.log("id is",e.target.id);
+            })//event listener
+          });// foreach end
       </script>
    
 </body>
